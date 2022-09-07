6e2008762c08f0ace1ee22b872c4076a1df880a3
Pack <packaq3@gmail.com>
2022-09-07 13:35:43 +0700

    Initial Commit

---
+++ ProfileCard/ProfileCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileCardApp.swift
+//  ProfileCard
+//
+//  Created by Pipat on 7/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct ProfileCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
